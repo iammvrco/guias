8b5408add2c57db29f98316c462d5309d32753ef
Marco Antonio <marco.vzz@outlook.com>
2021-04-04 17:59:30 -0500

    hola

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,11 @@
+import { Cliente } from "./cliente";
+
+export const CLIENTES: Cliente[] = [
+    {id: 1,nombre: 'Marco', apellido: 'Vázquez', email: 'marco.vzz@outlook.com', createAt: '2021-04-04'},
+    {id: 2,nombre: 'Marco', apellido: 'Rivera', email: 'marco.riv@outlook.com', createAt: '2021-04-02'},
+    {id: 3,nombre: 'Judith', apellido: 'Candelaria', email: 'judith.vzz@outlook.com', createAt: '2021-02-04'},
+    {id: 4,nombre: 'Alejandra', apellido: 'Vázquez', email: 'ale.vzz@outlook.com', createAt: '2021-04-04'},
+    {id: 5,nombre: 'Marco', apellido: 'Antonio', email: 'marco.ant@outlook.com', createAt: '2021-04-04'},
+    {id: 6,nombre: 'Antonio', apellido: 'Vázquez', email: 'antonio.vzz@outlook.com', createAt: '2021-04-04'},
+    {id: 7,nombre: 'Alejandra', apellido: 'Sánchez', email: 'ale.san@outlook.com', createAt: '2021-09-04'},
+  ]
