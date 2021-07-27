fa8e5333dfc8536250252808f9aab4b87181f5fb
Marco Antonio <marco.vzz@outlook.com>
2021-07-27 13:51:45 -0500

    Si no encuentran lo que necesitan pregunten lo que necesiten saber y yo les digo si está en el código

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,20 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { DirectivaComponent } from './directiva/directiva.component';
-import { ClientesComponent } from './clientes/clientes.component'
+import { ClientesComponent } from './clientes/clientes.component';
+import { ClienteService } from './clientes/cliente.service';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { CarruselComponent } from './carrusel/carrusel.component';
+import { HelpComponent } from './help/help.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/clientes', pathMatch: 'full'},
+  {path: 'directivas', component: DirectivaComponent},
+  {path: 'clientes', component: ClientesComponent},
+];
 
 @NgModule({
   declarations: [
@@ -13,12 +26,17 @@ import { ClientesComponent } from './clientes/clientes.component'
     HeaderComponent,
     FooterComponent,
     DirectivaComponent,
-    ClientesComponent
+    ClientesComponent,
+    HomeComponent,
+    CarruselComponent,
+    HelpComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [ClienteService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
